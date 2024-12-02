dfa0db6ecc640d9a01d26c499facc4aa9fbdfcf9
Asadbek Zaynobiddinov <asadbekzaynobiddinov51@gmail.com>
2024-12-02 09:35:55 +0500

    Logtail implement qilindi

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,22 @@
-import { createLogger, transports, format } from "winston"
+import { Logtail } from "@logtail/node";
+import { LogtailTransport } from "@logtail/winston";
+import { createLogger, format } from "winston";
+import { config } from "dotenv";
+
+
+config()
+
+
+const logtail = new Logtail(process.env.LOGGER_TOKEN)
 
 export const logger = createLogger({
-    level: "silly",
-    format: format.combine(
-        format.timestamp(),
-        // format.json(),
-        format.prettyPrint(),
-        format.colorize({ all: true }),
-    ),
-    transports: [
-        new transports.Console(),
-        new transports.File({ filename: "application.log" }),
-    ],
+  level: "silly",
+  format: format.combine(
+    format.timestamp(),
+    format.json(),
+    format.colorize({all: true})
+  ),
+  transports: [
+    new LogtailTransport(logtail)
+  ]
 })
